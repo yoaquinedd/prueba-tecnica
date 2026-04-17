549b7f57f82c671e478e26066309f1745865f87a
yoaquinedd <joaquinlolvlad@gmail.com>
2026-04-17 13:31:00 -0400

    first commit

---
+++ src/main/java/com/prueba/creacion_usuarios/CreacionUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.creacion_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreacionUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreacionUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/creacion_usuarios/CreacionUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.creacion_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreacionUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
